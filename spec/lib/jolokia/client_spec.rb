fef75dbd37fc6b34215eb1ebd54a5865f9ac1008
Tower He <towerhe@gmail.com>
2012-06-28 17:34:35 +0800

    Supported to writing attributes and excuting operations

--- spec/lib/jolokia/client_spec.rb
+++ spec/lib/jolokia/client_spec.rb
@@ -11,17 +11,18 @@ describe Jolokia::Client do
   end 
 
   describe '#request' do
+    shared_examples 'Request executed successfully' do
+      it 'responds with status 200' do
+        response['status'].should == 200
+      end
+    end
+
     let(:jolokia) { Jolokia::Client.new(url: url) }
-    let(:options) do
-      {
-        'type' => 'read',
-        'mbean' => 'java.lang:type=Memory',
-        'attribute' => attribute 
-      }
-    end
+
     let(:response) do
-      jolokia.request options
+      jolokia.request :post, options
     end
+
     let(:response_body) do
       File.new(File.join(File.dirname(__FILE__),
                          '../../responses', fake_response_body))
@@ -29,54 +30,100 @@ describe Jolokia::Client do
 
     before do
       stub_request(:post, url).
-        with(
-          body: Oj.dump(options),
-          headers: {
-            'Accept' => '*/*',
-            'Content-Type' => 'application/json',
-            'User-Agent' => 'Ruby'
-          }
-      ).to_return(body: response_body,
-                  status: 200)
+        with(body: Oj.dump(options)).to_return(body: response_body,
+                                               status: 200)
     end
 
-    context 'when reading a single attribute' do
-      let(:attribute) { 'HeapMemoryUsage' }
-      let(:fake_response_body) { 'heap_memory_usage.json' }
-
-      it 'responds with status 200' do
-        response['status'].should == 200
-      end
-
-      it 'responds the heap memory usage' do
-        response['value'].should == {
-          'max' => 477233152,
-          'committed' => 190382080,
-          'init' => 134217728,
-          'used' => 116851464
+    context 'when reading attributes' do
+      let(:options) do
+        {
+          'type' => 'read',
+          'mbean' => 'java.lang:type=Memory',
+          'attribute' => attribute 
         }
       end
-    end
 
-    context 'when reading multi-attributes' do
-      let(:attribute) { ['HeapMemoryUsage', 'NonHeapMemoryUsage'] }
-      let(:fake_response_body) { 'heap_memory_usage_and_none.json' }
+      context 'only a single attribute' do
+        let(:attribute) { 'HeapMemoryUsage' }
+        let(:fake_response_body) { 'heap_memory_usage.json' }
 
-      it 'responds HeapMemoryUsage and NonHeapMemoryUsage' do
-        response['value'].should == {
-          'NonHeapMemoryUsage' => {
-            'max' => 184549376,
-            'committed' => 88866816,
-            'init' => 24313856,
-            'used' => 88016336
-          },
-          'HeapMemoryUsage' => {
+        it_should_behave_like 'Request executed successfully'
+
+        it 'responds the heap memory usage' do
+          response['value'].should == {
             'max' => 477233152,
-            'committed' => 190447616,
+            'committed' => 190382080,
             'init' => 134217728,
-            'used' => 104668904
+            'used' => 116851464
           }
+        end
+      end
+
+      context 'multi-attributes' do
+        let(:attribute) { ['HeapMemoryUsage', 'NonHeapMemoryUsage'] }
+        let(:fake_response_body) { 'heap_memory_usage_and_none.json' }
+
+        it_should_behave_like 'Request executed successfully'
+
+        it 'responds HeapMemoryUsage and NonHeapMemoryUsage' do
+          response['value'].should == {
+            'NonHeapMemoryUsage' => {
+              'max' => 184549376,
+              'committed' => 88866816,
+              'init' => 24313856,
+              'used' => 88016336
+            },
+            'HeapMemoryUsage' => {
+              'max' => 477233152,
+              'committed' => 190447616,
+              'init' => 134217728,
+              'used' => 104668904
+            }
+          }
+        end
+      end
+    end
+
+    context 'when writing an attribute' do
+      let(:options) do
+        {
+          'type' => 'write',
+          'mbean' => 'java.lang:type=ClassLoading',
+          'attribute' => 'Verbose',
+          'value' => true
         }
+      end
+      let(:fake_response_body) { 'set_class_loading_verbose.json' }
+
+      it_should_behave_like 'Request executed successfully'
+    end
+
+    context 'when executing JMX operations (exec)' do
+      context 'without any params' do
+        let(:options) do
+          {
+            'type' => 'exec',
+            'mbean' => 'java.lang:type=Memory',
+            'operation' => 'gc'
+          }
+        end
+        let(:fake_response_body) { 'exec_memory_gc.json' }
+
+        it_should_behave_like 'Request executed successfully'
+      end
+
+      context 'with params' do
+        let(:options) do
+          {
+            'type' => 'exec',
+            'mbean' => 'java.lang:type=Threading',
+            'operation' => 'dumpAllThreads',
+            'arguments' => [true, true]
+          }
+        end
+        let(:fake_response_body) { 'exec_dump_all_threads.json' }
+
+        it_should_behave_like 'Request executed successfully'
       end
     end
   end
